a31bac65e77a572d9bf53dcfaf60f0381fbd36b6
Dominik <eitler.domi@gmail.com>
2021-04-02 18:18:07 +0200

    added landing page

--- src/routes/homepage.ts
+++ src/routes/homepage.ts
@@ -2,11 +2,6 @@ import express, { NextFunction, Request, Response } from 'express';
 
 const router = express.Router();
 
-const homePage = ((req: Request, res: Response, next: NextFunction) => {
-    res.send('Coming soon!');
-});
+router.use('/', express.static('resources/website'));
 
-router.get('/', homePage);
-router.get('/about', homePage);
-router.get('/donate', homePage);
 export default router;
